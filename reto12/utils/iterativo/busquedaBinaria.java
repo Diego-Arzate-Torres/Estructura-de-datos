02f2b1dc51b36b1e77e51f7c7b8ba9e8a56365c7
Diego Arzate <122845220+Diego-Arzate-Torres@users.noreply.github.com>
2023-11-04 21:12:13 -0600

    Reto 12
    Ya ahora si esta

--- reto12/utils/iterativo/busquedaBinaria.java
+++ reto12/utils/iterativo/busquedaBinaria.java
@@ -1,7 +1,16 @@
 package modulo3.reto12.utils.iterativo;
-
+/**
+ * Esta clase contiene un método para realizar una búsqueda binaria en un arreglo.
+ */
 public class busquedaBinaria {
-    int busquedaBinaria(int arr[], int x) {
+    /**
+     * Realiza una búsqueda binaria en el arreglo dado para encontrar el valor especificado.
+     *
+     * @param arr El arreglo en el que buscar. Debe estar ordenado de menor a mayor.
+     * @param x El valor a buscar en el arreglo.
+     * @return El índice del valor en el arreglo, o -1 si el valor no se encuentra en el arreglo.
+     */
+    public int busquedaBinaria(int arr[], int x) {
         int l = 0, r = arr.length - 1;
         while (l <= r) {
             int mid = l + (r - l) / 2;
@@ -13,5 +22,20 @@ public class busquedaBinaria {
                 r = mid - 1;
         }
         return -1;
+    }
+    /**
+     * Método principal para probar el método de búsqueda binaria.
+     *
+     * @param args Argumentos de la línea de comandos. No se utilizan en este método.
+     */
+    public static void main(String args[]) {
+        busquedaBinaria bbi = new busquedaBinaria();
+        int arr[] = {2, 3, 4, 10, 40};
+        int x = 10;
+        int resultado = bbi.busquedaBinaria(arr, x);
+        if (resultado == -1)
+            System.out.println("El elemento no está presente en el arreglo");
+        else
+            System.out.println("El elemento se encuentra en el índice " + resultado);
     }
 }
--- reto12/utils/recursivo/Fibonaccio.java
+++ reto12/utils/recursivo/Fibonaccio.java
@@ -1,10 +1,33 @@
 package modulo3.reto12.utils.recursivo;
-
+/**
+ * Esta clase contiene un método para calcular los primeros n números de la serie de Fibonacci.
+ */
 public class Fibonaccio {
-    static int fibonacci(int n) {
-        if (n <= 1)
-            return n;
+    /**
+     * Calcula el n-ésimo número de la serie de Fibonacci.
+     *
+     * @param n El índice del número en la serie de Fibonacci a calcular.
+     * @return El n-ésimo número de la serie de Fibonacci.
+     */
+    public static int fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return 1;
         else
             return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    /**
+     * Método principal para probar el método fibonacci.
+     *
+     * @param args Argumentos de la línea de comandos. No se utilizan en este método.
+     */
+    public static void main(String args[]) {
+        int n = 0; // Número de términos a imprimir
+        System.out.println("Los primeros " + (n) + " términos de la serie de Fibonacci son: ");
+
+        for (int i = 1; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
     }
 }
