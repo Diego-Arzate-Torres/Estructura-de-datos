4fe963ba88611c1ae7079deec8a9453125d362a1
Diego Arzate <122845220+Diego-Arzate-Torres@users.noreply.github.com>
2023-11-11 19:19:31 -0600

    reto 13 terminado
    Aqui esta el reto 13 terminado

--- reto13/main.java
+++ reto13/main.java
@@ -1,11 +1,16 @@
-package reto13;
+package modulo3.reto13;
+
+import static modulo3.reto13.uitls.mediana.calcularMediana;
 
 public class main {
-
-    public static void main(String args[]){
-
-        new ochoreinas();
+    /**
+     * Este es el método principal que utiliza el método calcularMediana.
+     *
+     * @param args Sin usar.
+     */
+    public static void main(String[] args) {
+        double[] numeros = {7, 2, 3, 1, 4, 5.2, 6};
+        double mediana = calcularMediana(numeros);
+        System.out.println("La mediana es: " + mediana);
     }
-
-
 }
